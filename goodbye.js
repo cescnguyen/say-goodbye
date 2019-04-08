cf4bc9a4c9c5992da206ab715e3ed28a14f4585d
cescnguyen <ducvnmail@gmail.com>
2019-04-08 23:38:45 +0700

    init project SayGoodBye

---
+++ goodbye.js
@@ -0,0 +1,14 @@
+var chalk = require("chalk")
+
+
+function GoodBye(name) {
+    this.name = name;
+}
+
+
+GoodBye.prototype.sayGoodBye = function () {
+    console.log('good bye ', chalk.green(this.name))
+}
+
+
+module.exports = GoodBye;
---
+++ main.js
@@ -0,0 +1,5 @@
+var GoodBye = require('./goodbye')
+
+
+var goodBye = new GoodBye('Mr. Dog');
+goodBye.sayGoodBye();
